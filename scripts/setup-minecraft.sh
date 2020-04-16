b0ca5076e77469d1df7a5d9484b227735b685631
regnare <regnare@gmail.com>
2020-04-16 10:50:35 -0400

    Fixed typo in the start-minecraft.sh file. Also moved the minecraft server URL into a variable.

--- scripts/setup-minecraft.sh
+++ scripts/setup-minecraft.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 MC_SCRIPT="start-minecraft.sh"
+MC_URL="https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar"
 
 # Install openjdk
 apt-get install software-properties-common 
@@ -9,15 +10,17 @@ apt-get install openjdk-8-jre -y
 
 # Download minecraft server
 mkdir minecraft 
-wget -O ./minecraft/minecraft_server.jar https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
+wget -O ./minecraft/minecraft_server.jar "$MC_URL"
 
 (
 cat <<'EOF'
 #!/bin/bash
 cd minecraft
-java -Xmx 1024M -Xms1024M -jar minecraft_server.jar nogui
+java -Xmx1024M -Xms1024M -jar minecraft_server.jar nogui
 EOF
 ) > "$MC_SCRIPT"
 chmod +x "$MC_SCRIPT"
+
+echo "eula=true" > ./minecraft/eula.txt
 
 echo "Run ./$MC_SCRIPT to start the server."
